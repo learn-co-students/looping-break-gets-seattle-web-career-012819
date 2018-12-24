222a266bcd297fda3534012c067385c2cc038490
Kathleen Carrel <katiecarrel@gmail.com>
2018-12-24 00:22:15 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,10 @@
 
-loop do
-  puts "What is the spell that enacts levitation?"
-    answer = gets.chomp!
+#def levitation_quiz
+	loop do
+	  puts "What is the spell that enacts levitation?"
+	  answer = gets.chomp!
 	  break if answer == "Wingardium Leviosa"
-end
+	 end
+#end
+
 
